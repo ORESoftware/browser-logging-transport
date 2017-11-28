97334a706156a26b2079daefb73ed1442782f659
Olegzandr Denman <alex@oresoftware.com>
2017-11-27 21:26:39 -0800

    pdev:set

--- index.ts
+++ index.ts
@@ -1,48 +1,47 @@
 !function (root, factory) {
 
-    if ('function' === typeof define && define.amd) {
-        define('BLT', [], function () {
-            return (root.mymodule = factory())
-        });
-    }
+  if ('function' === typeof define && define.amd) {
+    define('BLT', [], function () {
+      return (root.mymodule = factory())
+    });
+  }
 
-    else if ('object' === typeof module && module.exports) {
-        module.exports = factory();
-    }
+  else if ('object' === typeof module && module.exports) {
+    module.exports = factory();
+  }
 
-    else {
-        root['BLT'] = factory();
-    }
-
+  else {
+    root['BLT'] = factory();
+  }
 
 }(this, function () {
 
-    'use strict';
-    return function (boundTransportFn: (a: any, key: string) => void) {
+  'use strict';
+  return function (boundTransportFn: (a: any, key: string) => void) {
 
-        const str = String(boundTransportFn);
+    const str = String(boundTransportFn);
 
-        if(/console\.log\(/.test(str)){
-            throw new Error(' => If you call console.log() inside the bound function, you will get a stack overflow error.');
-        }
-
-        Object.keys(console).forEach(function (key) {
-
-            let f;
-
-            if ((f = console[key]) && typeof f === 'function') {
-
-                console[key] = function () {
-
-                    const data = Object.values(arguments).join(' ');
-                    boundTransportFn(data, key);
-
-                    f.apply(console, arguments);
-                };
-            }
-        });
-
+    if (/console\.log\(/.test(str)) {
+      throw new Error(' => If you call console.log() inside the bound function, you will get a stack overflow error.');
     }
+
+    Object.keys(console).forEach(function (key) {
+
+      let f;
+
+      if ((f = console[key]) && typeof f === 'function') {
+
+        console[key] = function () {
+
+          const data = Object.values(arguments).join(' ');
+          boundTransportFn(data, key);
+
+          f.apply(console, arguments);
+        };
+      }
+    });
+
+  }
 });
 
 
